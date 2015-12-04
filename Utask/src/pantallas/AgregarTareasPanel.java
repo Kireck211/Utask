3e1445776ee8b3854453387ca5e1e104eef56d30
Kireck211 <erickdesantiago11@gmail.com>
2015-12-04 13:37:05 -0600

    Arreglo todo de interfaz, existen detalles

--- Utask/src/pantallas/AgregarTareasPanel.java
+++ Utask/src/pantallas/AgregarTareasPanel.java
@@ -26,12 +26,10 @@ public class AgregarTareasPanel extends JPanel{
 	private JLabel descripcion;
 	private JLabel realizada;
 	private String[] opciones = {"Realizada", "No realizada"};
-	private JTextArea tituloA;
 	private JTextArea asignaturaA;
 	private JTextArea nombreA;
 	private JTextArea descripcionA;
 	private JComboBox lista;
-	private Vector<Tarea> tareas;
 	private String nombre_jp;
 	private String asignatura_jp;
 	private String descripcion_jp;
@@ -46,9 +44,17 @@ public AgregarTareasPanel() {
 	setLayout(new BorderLayout());
 	titulo = new JLabel(new ImageIcon("AgregarTareas.png"));
 	asignatura = new JLabel(new ImageIcon("Asignaturaa.png"));
+	asignaturaA = new JTextArea();
+	asignaturaA.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.GRAY));
 	nombre = new JLabel(new ImageIcon("nombree.png"));
+	nombreA = new JTextArea();
+	nombreA.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.GRAY));
 	descripcion = new JLabel(new ImageIcon("Descripcion.png"));
+	descripcionA = new JTextArea();
+	descripcionA.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.GRAY));
 	realizada = new JLabel(new ImageIcon("Realizada.png"));
+	lista = new JComboBox<String>(opciones);
+	lista.setSelectedIndex(0); 	
 	GridBagConstraints gc = new GridBagConstraints();
 	ok = new JButton(new ImageIcon("ok.png"));
 	cancel = new JButton(new ImageIcon("cancel.png"));
@@ -58,9 +64,7 @@ public AgregarTareasPanel() {
 	ok.setBorderPainted(false);
 	cancel.setBorderPainted(false);
 	setLayoutPanel(gc);
-	
-	lista = new JComboBox(opciones);
-	lista.setSelectedIndex(0); 	
+
 
 }
 public void setLayoutPanel(GridBagConstraints gc){
@@ -73,6 +77,7 @@ public void setLayoutPanel(GridBagConstraints gc){
 	gc.anchor= GridBagConstraints.CENTER;
 	add(titulo, gc);
 
+	
 	///////// Next row ///////
 	gc.gridy++;
 	gc.gridx = 0;
@@ -80,28 +85,40 @@ public void setLayoutPanel(GridBagConstraints gc){
 	gc.anchor= GridBagConstraints.WEST;
 	gc.insets = new Insets(0, 0, 0, 0);
 	add(nombre,gc);
+
 	gc.gridx = 1;
-	JLabel nombre_jl = new JLabel(nombre_jp);
-	add(nombre_jl,gc);
+	gc.ipadx = 200;
+	gc.insets = new Insets(0, 0, 0, 50);
+	add(nombreA, gc);
 
 	///////// Next row ///////
 	gc.gridx = 0;
 	gc.gridy++;
 	add(asignatura,gc);
+	
 	gc.gridx = 1;
-	JLabel asignatura_jl = new JLabel(asignatura_jp);
-	add(asignatura_jl, gc);
+	gc.ipadx = 200;
+	gc.insets = new Insets(0, 0, 0, 50);
+	add(asignaturaA, gc);
 	
 	gc.gridy++;
 	gc.gridx = 0;
 	add(descripcion,gc);
+
+	
 	gc.gridx = 1;
-	JLabel descripcion_jl = new JLabel(descripcion_jp);
-	add(descripcion_jl,gc);
+	gc.ipadx = 200;
+	gc.insets = new Insets(0, 0, 0, 50);
+	add(descripcionA, gc);
 	
 	gc.gridx = 0;
 	gc.gridy++;
 	add(realizada,gc);
+	
+	gc.gridx = 1;
+	gc.ipadx = 180;
+	gc.insets = new Insets(0, 0, 0, 20);
+	add(lista,gc);
 	
 	gc.gridy++;
 	gc.ipadx = 0;
@@ -112,9 +129,7 @@ public void setLayoutPanel(GridBagConstraints gc){
 	gc.gridx = 1;
 	add(cancel,gc);
 	
-	/*gc.ipadx = 180;
-	gc.insets = new Insets(0, 0, 0, 20);
-	add(lista,gc);*/
+
 
 	
 	
