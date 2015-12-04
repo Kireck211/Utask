4ec2b849b0691ce4309181f4867a89d0f99b1cd3
JossyEsparza <jossyesparza@gmail.com>
2015-12-03 20:49:32 -0600

    Interfaz

--- Utask/src/pantallas/AgregarClasePanel.java
+++ Utask/src/pantallas/AgregarClasePanel.java
@@ -12,6 +12,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 import planeacion.Clase;
 import planeacion.Materia;
@@ -26,7 +27,17 @@ public class AgregarClasePanel extends JPanel{
 	private JLabel profesor;
 	private JLabel dSemana;
 	private JLabel examenes;
+<<<<<<< HEAD
+	private JTextArea inicioLinea;
+	private JTextArea nombreLinea;
+	private JTextArea finLinea;
+	private JTextArea ubicacionLinea;
+	private JTextArea profesorLinea;
+	private JTextArea dSemanaLinea;
+	private JTextArea examenesLinea;
+=======
 	private Vector<Clase> clases;
+>>>>>>> 24bdcec29919ddadd8567eb8a047a60a2f5b213a
 
 public AgregarClasePanel() {
 	setBackground(new Color(255, 255, 255));
@@ -34,18 +45,32 @@ public AgregarClasePanel() {
 	setLayout(new BorderLayout());
 	titulo = new JLabel(new ImageIcon("AgregarClase.png"));
 	inicio = new JLabel(new ImageIcon("Inicio.png"));
+	inicioLinea = new JTextArea();
+	inicioLinea.setBorder(BorderFactory.createEtchedBorder());
 	nombre = new JLabel(new ImageIcon("nombree.png"));
+	nombreLinea = new JTextArea();
+	nombreLinea.setBorder(BorderFactory.createEtchedBorder());
 	fin = new JLabel(new ImageIcon("Fin.png"));
+	finLinea = new JTextArea();
+	finLinea.setBorder(BorderFactory.createEtchedBorder());
 	ubicacion = new JLabel(new ImageIcon("Ubicacion.png"));
+	ubicacionLinea = new JTextArea();
+	ubicacionLinea.setBorder(BorderFactory.createEtchedBorder());
 	profesor = new JLabel(new ImageIcon("profesores.png"));
+	profesorLinea = new JTextArea();
+	profesorLinea.setBorder(BorderFactory.createEtchedBorder());
 	dSemana = new JLabel(new ImageIcon("dSemana.png"));
+	dSemanaLinea = new JTextArea();
+	dSemanaLinea.setBorder(BorderFactory.createEtchedBorder());
 	examenes = new JLabel(new ImageIcon("examen.png"));
+	examenesLinea = new JTextArea();
+	examenesLinea.setBorder(BorderFactory.createEtchedBorder());
 	
 	setLayoutPanel();
 	
 	String nombre = JOptionPane.showInputDialog("Nombre de la tarea");
 	String asignatura = JOptionPane.showInputDialog("Nombre de la asignatura");
-	String descripcion = JOptionPane.showInputDialog("Escriba la descripción");
+	String descripcion = JOptionPane.showInputDialog("Escriba la descripciÃ³n");
 //	tareas.addElement(new Tarea(new Materia(asignatura),nombre,descripcion));
 
 }
@@ -55,6 +80,7 @@ public void setLayoutPanel(){
 
 	gc.weightx = 5;
 	gc.weighty = 5;
+	gc.gridwidth=2;
 	gc.gridx = 0;
 	gc.gridy = 0;
 	gc.anchor= GridBagConstraints.CENTER;
@@ -62,32 +88,88 @@ public void setLayoutPanel(){
 
 	///////// Next row ///////
 	gc.gridy++;
+	gc.gridwidth=1;
+	gc.weightx = 2.5;
 	gc.gridx = 0;
 	gc.ipadx = 0;
 	gc.anchor= GridBagConstraints.WEST;
 	gc.insets = new Insets(0, 0, 0, 0);
 	add(nombre,gc);
-
+	
+	gc.gridx = 1;
+	gc.ipadx = 10;
+//	gc.ipadx = 180;
+	add(nombreLinea,gc);
 
 	///////// Next row ///////
 	gc.gridy++;
+	gc.gridx = 0;
+	gc.ipadx = 0;
+	gc.insets = new Insets(0, 0, 0, 0);
 	add(inicio,gc);
 	
-	gc.gridy++;
-	add(fin,gc);
+	gc.gridx = 1;
+	gc.ipadx = 10;
+//	gc.insets = new Insets(0, 0, 0, 20);
+	add(inicioLinea, gc);
 	
 	gc.gridy++;
+	gc.gridx = 0;
+	gc.ipadx = 0;
+	gc.insets = new Insets(0, 0, 0, 0);
+	add(fin,gc);
+
+	gc.gridx = 1;
+	gc.ipadx = 10;
+//	gc.insets = new Insets(0, 0, 0, 20);
+	add(finLinea, gc);
+	
+	
+	gc.gridy++;
+	gc.gridx = 0;
+	gc.ipadx = 0;
+	gc.insets = new Insets(0, 0, 0, 0);
 	add(ubicacion,gc);
 	
+	gc.gridx = 1;
+	gc.ipadx = 10;
+//	gc.insets = new Insets(0, 0, 0, 20);
+	add(ubicacionLinea, gc);
+	
+	
 	gc.gridy++;
+	gc.gridx = 0;
+	gc.ipadx = 0;
+	gc.insets = new Insets(0, 0, 0, 0);
 	add(profesor,gc);
 	
-	gc.gridy++;
-	add(examenes,gc);
+	gc.gridx = 1;
+	gc.ipadx = 10;
+//	gc.insets = new Insets(0, 0, 0, 20);
+	add(profesorLinea, gc);
 	
 	gc.gridy++;
-	gc.insets = new Insets(0, 0, 0, 20);
+	gc.gridx = 0;
+	gc.ipadx = 0;
+	gc.insets = new Insets(0, 0, 0, 0);
+	add(examenes,gc);
+	
+	gc.gridx = 1;
+	gc.ipadx = 10;
+//	gc.insets = new Insets(0, 0, 0, 20);
+	add(examenesLinea, gc);
+	
+	
+	gc.gridy++;
+	gc.ipadx = 0;
+	gc.gridx = 0;
+//	gc.insets = new Insets(0, 0, 0, 20);
 	add(dSemana,gc);
+	
+	gc.gridx = 1;
+	gc.ipadx = 10;
+	add(dSemanaLinea, gc);
+	
 
 	
 
