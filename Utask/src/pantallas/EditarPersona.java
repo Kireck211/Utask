025f5a82f3172219b610afc0706483bf7356d334
AldoMoyPB <aldomoypb@gmail.com>
2015-12-03 20:29:49 -0600

    Update EditarPersona.java

--- Utask/src/pantallas/EditarPersona.java
+++ Utask/src/pantallas/EditarPersona.java
@@ -1,5 +1,7 @@
 package pantallas;
 
+import java.awt.Dimension;
+
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -8,7 +10,13 @@ public class EditarPersona extends JFrame {
 	private JButton cancel;
 	
 	public EditarPersona() {
-		cancel = new JButton(new ImageIcon("cancel"));
+		cancel = new JButton(new ImageIcon("cancel.png"));
+		cancel.setContentAreaFilled(false);
+		add(cancel);
+		
+		setLocationRelativeTo(null);
+		setSize(50, 50);
+		setIconImage(new ImageIcon("UtaskLogoo.png").getImage());
 		
 	}
 }
